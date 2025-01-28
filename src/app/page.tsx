12808bd6288c41e908b459020d86459e9ab8e9d1
Iryna Oliinyk <arairishka2@gmail.com>
2025-01-28 21:51:59 +0100

    part 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 import StatusLabel, {Status} from "../components/status-label";
-import clsx from "clsx";
+// import clsx from "clsx";
 
 import "./globals.css";
+import AddCompanyButton from "@/components/add-company-button";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl text-blue-600">Home page </h1>
-        <StatusLabel status={Status.Activ}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActiv}>Not activ</StatusLabel>
-        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl ">Home page </h1>
+       <AddCompanyButton/>
+        {/*<StatusLabel status={Status.Active}>Active</StatusLabel>*/}
+        {/*<StatusLabel status={Status.NotActive}>Not activ</StatusLabel>*/}
+        {/*<StatusLabel status={Status.Pending}>Pending</StatusLabel>*/}
+        {/*<StatusLabel status={Status.Suspended}>Suspended</StatusLabel>*/}
 
     </main>
   );
